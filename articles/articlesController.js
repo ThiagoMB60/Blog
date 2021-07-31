64988a3abb9fb303b11bda8023db7b87a3503de7
ThiagoMB60 <thiagomoreira60@gmail.com>
2021-07-31 13:51:08 -0300

    a

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,14 +1,58 @@
 const express = require("express");
 const router = express.Router();
 const category = require("../categories/category");
+const article = require("./article");
+const slugify = require("slugify");
 
-router.get("/articles",(req, res) =>{
-	res.send("ROTA DE ARTIGOS");
+//listar artigos
+router.get("/admin/articles",(req, res) =>{
+	article.findAll({
+		include: [{model: category}]
+	}).then((articles) => {
+		res.render("admin/articles/index", {articles: articles});
+	});	
 });
 
+//deletar artigos
+router.post("/articles/delete", (req, res) =>{
+	var id = req.body.id;
+	if(id != undefined){
+		if(isNaN(id)){
+			res.redirect("/admin/articles");
+		}else{
+			article.destroy({
+				where: {
+					id: id
+				}
+			}).then(() => {
+				res.redirect("/admin/articles");
+			});
+		}
+	}else{
+		res.redirect("/admin/articles");
+	}
+});
+
+//criar novos artigos
 router.get("/admin/articles/new",(req, res) =>{
 	category.findAll().then(categories =>{
 		res.render("admin/articles/new", {categories: categories});
+	});
+});
+
+//salvar novos artigos
+router.post("/articles/save", (req, res) => {
+	var title = req.body.title;
+	var body = req.body.body;
+	var category = req.body.category;
+
+	article.create({
+		title: title,
+		slug: slugify(title),
+		body: body,
+		categoryId: category
+	}).then(() => {
+		res.redirect("/admin/articles");
 	});
 });
 
--- database/database.js
+++ database/database.js
@@ -1,7 +1,8 @@
 const sequelize = require("sequelize");
 const connection = new sequelize('blog', 'root', '12345', {
 	host: 'localhost',
-	dialect: 'mysql'
+	dialect: 'mysql',
+	timezone: "-03:00"
 });
 
 module.exports = connection;
